58c9891910d6198a59953afb23ff16c6c5a45af7
cheykoff <mail@michael-schoelkopf.de>
2019-09-24 22:11:26 +0200

    first working version of anagrams. Needs refactoring.

--- 12-2-anagrams.py
+++ 12-2-anagrams.py
@@ -1,23 +1,56 @@
-'''
-requirements
-input: word list as a txt file
-output: print all words from txt file that have anagrams with all their anagrams within the word list
-
-solution
-read a txt file and store the words in a list
-search each word in the dictionary (bisect search)
-	search for each word that is an anagram
-print word and its anagrams
-
-ideas:
-dictionary with primes for each letter and value is a list of words
-'''
-
 def read_word_list_into_list():
-	l = []
+	word_list = []
 	fin = open('anagrams-test.txt')
 	for line in fin:
-		l.append(line.strip())
-	print(l)
+		word_list.append(line.strip())
+	return word_list
 
-read_word_list_into_list()
+def letter_to_number(char):
+	char = char.lower()
+	number = ord(char) - 97
+	return number
+
+def get_nth_prime(n):
+	primes = []
+	i = 2
+	count = 0
+	while count <= n:
+		j = 2
+		is_prime = True
+		while j < i:
+			if i % j == 0:
+				is_prime = False	
+			j += 1
+		if is_prime:
+			primes.append(i)
+			count += 1
+		i += 1
+	return primes[n]
+
+def get_prime_mult_for_string(s):
+	prime_mult = 1
+	for char in s:
+		prime_mult = prime_mult * get_nth_prime(letter_to_number(char))
+	return prime_mult
+
+def map_word_list_to_prime_mult(word_list):
+	l = []
+	for word in word_list:
+		t = (word, get_prime_mult_for_string(word))
+		l.append(t)
+	return l
+
+def find_anagrams(l):
+	for item in l:
+		k = []
+		for item2 in l:
+			if item[1] == item2[1] and item != item2:
+				k.append(item2[0])
+		if len(k) > 0:
+			print(item[0], 'has anagrams', k) 
+		else:
+			print(item[0], 'has no anagrams') 
+	
+word_list = read_word_list_into_list()	
+prime_mult_list = map_word_list_to_prime_mult(word_list)
+find_anagrams(prime_mult_list)
