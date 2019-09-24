b39462f5bc25d3269de07d870adfe2a0499ad7a9
cheykoff <mail@michael-schoelkopf.de>
2019-09-24 21:17:40 +0200

    add 12-2-anagrams.py and txt file

---
+++ 12-2-anagrams.py
@@ -0,0 +1,14 @@
+'''
+requirements
+input: word list as a txt file
+output: print all words from txt file that have anagrams with all their anagrams within the word list
+
+solution
+read a txt file and store the words in a list
+search each word in the dictionary (bisect search)
+	search for each word that is an anagram
+print word and its anagrams
+
+ideas:
+dictionary with primes for each letter and value is a list of words
+'''
