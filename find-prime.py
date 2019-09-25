f0e6a9f9351923d073226544e00e9462bc8f700a
cheykoff <mail@michael-schoelkopf.de>
2019-09-25 21:14:56 +0200

    moved all prints into comments and only return nth prime

--- find-prime.py
+++ find-prime.py
@@ -13,23 +13,25 @@ def get_nth_prime(n):
 	count = 0
 	while count <= n:
 		is_prime = True
-		print('current primes:', primes)
+		#print('current primes:', primes)
 		for prime in primes:
 			if prime <= number**0.5 and is_prime:
-				print('check prime', prime, 'for', number)
+				#print('check prime', prime, 'for', number)
 				if number % prime == 0:
-					print('number', number, 'can be divided by', prime, 'so it is not a prime')
+					#print('number', number, 'can be divided by', prime, 'so it is not a prime')
 					is_prime = False
 				else:
-					print('number', number, 'can not be divided by', prime, 'so it could be still a prime')
+					#print('number', number, 'can not be divided by', prime, 'so it could be still a prime')
+					pass
 		if is_prime:
-			print(number, 'is a prime')
+			#print(number, 'is a prime')
 			primes.append(number)
 			count += 1
 		else:
-			print(number, 'is no prime')
+			#print(number, 'is no prime')
+			pass
 		number += 1
-	return primes
+	return primes[n]
 
-primes = get_nth_prime(10)
+primes = get_nth_prime(1)
 print(primes)
