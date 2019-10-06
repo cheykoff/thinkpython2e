a04825d87565e41e3f5fd8bff2480980b711a162
cheykoff <mail@michael-schoelkopf.de>
2019-10-06 16:20:35 +0200

    Refactoring: Use descriptive naming

--- 12-3-metathesis-pairs.py
+++ 12-3-metathesis-pairs.py
@@ -5,50 +5,43 @@ output: all words which can be transformed into another word by changing two let
 
 solution approach:
 save words into dictionary with signature as key and anagrams as list {'acr': ['arc', 'car']}
-for each list of anagrams (=values of the dictionary) compare each word with the other words
+for each list of anagrams (=values of the dictionary) compare each word with all other words within the list
 if all but two letters are in the same place then it is a metathesis
-print the metatheses
+print the metathesis_pairs
 '''
 
-def read_list():
-	d = {}
+def read_file():
+	d_signature_anagrams = {}
 	with open('words-long.txt') as fin:
 		for line in fin:
 			word = line.strip()
 			if word.isalpha():
 				word = word.lower()
 				key = tuple(sorted(word))
-				d[key] = d.get(key,[]) + [word]
-	return d
+				d_signature_anagrams[key] = d_signature_anagrams.get(key,[]) + [word]
+	return d_signature_anagrams
 
-def go_through_dictionary(d):
-	count_metatheses = 0
-	list_of_words = list(d.values())
-	#print(list_of_words)
-	for words in list_of_words:
-		metatheses = find_metathesis(words)
-		if len(metatheses):
-			count_metatheses += len(metatheses)
-			print(metatheses)
-	print(int(count_metatheses/2), 'pairs of metatheses were found') 
+def find_and_print_metathesis_pairs_from_dictionary(d_signature_anagrams):
+	count_metathesis_pairs = 0
+	list_of_anagrams = list(d_signature_anagrams.values())
+	for anagrams in list_of_anagrams:
+		metathesis_pairs = find_metathesis_pairs_within_anagrams(anagrams)
+		if len(metathesis_pairs):
+			count_metathesis_pairs += len(metathesis_pairs)
+			print(metathesis_pairs)
+	print(int(count_metathesis_pairs/2), 'pairs of metathesis were found') 
 
-def find_metathesis(words):
-	metatheses = []
-	for i, word in enumerate(words):
-		for j, word in enumerate(words):
+def find_metathesis_pairs_within_anagrams(anagrams):
+	metathesis_pairs = []
+	for i, word in enumerate(anagrams):
+		for j, word in enumerate(anagrams):
 			diff_letter_count = 0
-			for k, letter in enumerate(words[i]):
-				if words[i][k] != words[j][k]:
+			for k, letter in enumerate(anagrams[i]):
+				if anagrams[i][k] != anagrams[j][k]:
 					diff_letter_count += 1
 			if diff_letter_count == 2:
-				metatheses.append((words[i],words[j]))
-	return metatheses
-'''
-def print_anagrams(d):
-	for key in d:
-		anagrams = d[key]
-		if len(anagrams) > 1:
-			print(d[key])
-'''
-dictionary = read_list()
-go_through_dictionary(dictionary)
+				metathesis_pairs.append((anagrams[i],anagrams[j]))
+	return metathesis_pairs
+
+d_signature_anagrams = read_file()
+find_and_print_metathesis_pairs_from_dictionary(d_signature_anagrams)
