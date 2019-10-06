6b14ecda0915bf85e71a35ded8d75cfa161dd92e
cheykoff <mail@michael-schoelkopf.de>
2019-10-06 22:16:11 +0200

    alternative solution approach

--- 12-4-remove-letters.py
+++ 12-4-remove-letters.py
@@ -9,7 +9,14 @@ d = {'ghost': ['ghos', 'ghot', 'ghst', 'gost', 'host']}
 after all words have this list
 start with 'a' (and then with 'I') and find words which have one letter more
 do this recursevily no more words are found
-go through these words and get the longest 
+go through these words and get the longest
+
+alternative:
+find all words which have either an 'I'or an 'a' in it. The rest of words are not suitable.
+sort all words by length
+start with the longest word and try to reduce it by one letter at a time
+if it works we have found the longest word (test words which are equally long)
+if not go the next word
 '''
 with open('words.txt') as file:
 	for line in file:
