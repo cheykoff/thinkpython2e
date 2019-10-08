3374ccd23a009c166659abf7fb2ec8528bc847b7
cheykoff <mail@michael-schoelkopf.de>
2019-10-08 23:34:06 +0200

    I get the words with its deviratatives. But time increases drastically with words in the list. Needs refactoring or even different approach.

--- 12-4-remove-letters.py
+++ 12-4-remove-letters.py
@@ -4,8 +4,8 @@ input: file with words (one per line)
 output: longest word which can be always reduced (one letter at a time) to a valid word until it only has one letter left
 
 solution approach:
-find for each word all potential words which can be derived from it by reducing each of its letters
-d = {'ghost': ['ghos', 'ghot', 'ghst', 'gost', 'host']}
+find for each word all words in the file which can be derived from it by reducing each of its letters
+d = {'ghost': ['ghot', 'gost', 'host']}
 after all words have this list
 start with 'a' (and then with 'I') and find words which have one letter more
 do this recursevily no more words are found
@@ -23,20 +23,27 @@ def create_dictionary():
 		d = {}
 		for line in file:
 			word = line.strip()
-			if len(word) == 2:
-				print(word)
+			if len(word) < 5:
 				d[word] = None
 	return d
 
 def reduce_word(word, d):
+	l = []
 	for key in d:
-		print(key)
-		if word[1:3] == key:
-			print(word)
+		for i in range(len(word)):
+			word2 = word[0:i] + word[i+1:len(word)]
+			if word2 == key:
+				l.append(key)
+	return l
 
-word = 'zye'
+def add_derived_words(d):
+	d2 = {}
+	for key in d:
+		value = reduce_word(key, d)
+		d2[key] = d2.get(key, []) + value
+	for key in d2:
+		if len(d2[key]):
+			print(key, d2[key])
+
 dictionary = create_dictionary()
-
-print(word)
-print(dictionary)
-reduce_word(word, dictionary)
+add_derived_words(dictionary)
