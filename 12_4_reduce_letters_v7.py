c174f932fd17e1c336ad32b273a9971b723658a2
cheykoff <mail@michael-schoelkopf.de>
2019-10-16 11:23:55 +0200

    Refactoring: add scaffolding to refactor

--- 12_4_reduce_letters_v7.py
+++ 12_4_reduce_letters_v7.py
@@ -1,17 +1,16 @@
 def create_dictionary():
 	d = {}
-	with open ('words.txt') as file:
+	with open ('words-test.txt') as file:
 		for line in file:
 			word = line.strip().lower()
 			d[word] = []
 	return d
 
 def get_children(d):
-	d2 = {}
 	for word in d:
 		children = derive_children(word, d)
-		d2[word] = children
-	return d2
+		d[word] = children
+	return d
 
 def derive_children(word, d):
 	children = []
@@ -22,16 +21,21 @@ def derive_children(word, d):
 			children.append(child)
 	return children
 
-def filter_for_parents(d2):
-	d3 = {}
-	for word in d2:
-		if word == '':
-			d3[word] = d2[word]
-		elif d2[word] != []:
-			d3[word] = d2[word]
-	return d3
-
-
+def reduce(word, d):
+	print('reduce word:', word)
+	if word in reducible_words:
+		print('is in reducible words', word, reducible_words[word])
+		return reducible_words[word]
+	print('is not yet in reducible_words')
+	reducible_children = []
+	children = derive_children(word,d)
+	print('word:', word, 'with children:', children)
+	for child in children:
+		if reduce(child, d):
+			reducible_children.append(child)
+			print('reducible_children:', reducible_children)
+			reducible_words[word] = reducible_children
+			return reducible_children
 
 def print_trail(word, d):
 	if word == '':
@@ -41,41 +45,15 @@ def print_trail(word, d):
 		child = d[word]
 		print_trail(child[0], d)
 
-d = create_dictionary()
-#print('dictionary d - all words')
-#print(d)
-
-d2 = get_children(d)
-#print('dictionary d2 all words with all children')
-#print(d2)
-
-d3 = filter_for_parents(d2)
-#print('dictionary d3 all words which can be reduced at least once')
-#print(d3)
+d = get_children(create_dictionary())
 
 reducible_words = {}
 reducible_words[''] = ['']
 
-def get_reducable_words(d):
-	for word in d:
-		reduce(word, d)
-
-def reduce(word, d):
-	if word in reducible_words:
-		return reducible_words[word]
-	res = []
-	children = derive_children(word,d)
-	for child in children:
-		if reduce(child, d):
-			res.append(child)
-	reducible_words[word] = res
-	return res
-
-for word in d3:
-	reduce(word, d3)
-
-#print('reducible_words')
-#print(reducible_words)
+for word in d:
+	#reduce(word, d)
+	print('------------------------')
+	print('result', reduce(word, d))
 
 max_length = 0
 for word in reducible_words:
